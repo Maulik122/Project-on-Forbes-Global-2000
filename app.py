7f291c2e998ccc47c63592cb8ee805438ad78260
Earth123123 <103982294+Earth123123@users.noreply.github.com>
2023-07-01 11:57:21 +0530

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.title(""" Forbes Global 2023""")
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib as plt
 import seaborn as sns
 import plotly.express as px
 
